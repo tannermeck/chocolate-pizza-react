ee0b6df472cd343fcd991888d0f6bac34098784a
TANNER MECK <steve.meck246@gmail.com>
2021-08-02 16:00:44 -0700

    body header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
+import Recipe from './Recipe.js';
 
 class App extends Component {
   render() { 
     return (
     <>
       <Header />
+      <Recipe />
     </>
     );
   }
